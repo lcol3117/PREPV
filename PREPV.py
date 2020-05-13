8ff29d402a7804c057d416f2e56f968a6367f2e4
lcol3117 <landoncol3117@gmail.com>
2020-05-13 14:04:20 +0000

    avoidance vector stuff

--- PREPV.py
+++ PREPV.py
@@ -24,9 +24,15 @@ class PREPV_agent:
     initialvector = self.calculateRectVector(selectedpoint, selectedregion)
     print("Local max is {} relative to my inital point. ".format(initialvector))
     newvector = self.epsilonModifyMagnitude(initialvector, epsilon)
-    print("Modified vector is {}. ".format(newvector))
-    respoint = self.vectorSum(newvector, selectedpoint)
-    return respoint
+    newpoint = self.vectorSum(selectedpoint, newvector)
+    print("New vector is {}. ".format(newvector))
+    region_min = self.selectRegionMin(epsilon)
+    print("I chose minimum region {}. ".format(region_min))
+    vec_a = self.calculateRectVector(region_min, newpoint)
+    vec_delta_b = self.calculateAvoidanceVector(vec_a, epsilon)
+    print("Avoidance vector is {}.  ".format(vec_delta_b))
+    chosenvector = self.vectorSum(region_min, vec_delta_b)
+    return chosenvector
   def calculateEpsilon(self):
     try: 
       qtablemax = ft.reduce(
@@ -34,7 +40,7 @@ class PREPV_agent:
         self.accdata,
         float("-inf")
       )
-      res = (qtablemax / self.mip) ** 2
+      res = (qtablemax / self.mip)
     except IndexError:
       res = 0
     self.prev_epsilon = res
@@ -51,8 +57,21 @@ class PREPV_agent:
         r[i+1] = r[i][:]
         r[i] = tmp[:]
     newranking = r[:]
-    newtop = newranking[0]
+    newtop = newranking[0][0]
     self.usedregions.append(newtop)
+    return newtop
+  def selectRegionMin(self, epsilon):
+    e2 = epsilon / 2
+    ls = self.accdata[:]
+    ranking = sorted(ls, key = lambda x : 0 - x[1])[:]
+    r = list(reversed(ranking))[:]
+    for i in range(len(ranking)-1):
+      if rnd.random() < e2:
+        tmp = r[i+1][:]
+        r[i+1] = r[i][:]
+        r[i] = tmp[:]
+    newranking = r[:]
+    newtop = newranking[0][0]
     return newtop
   def randomPoint(self):
     rndpt = map(
@@ -61,13 +80,16 @@ class PREPV_agent:
     )
     return list(rndpt)
   def calculateRectVector(self, point, region):
-    regionpt = region[0]
+    regionpt = region[:]
     r = list(range(self.dims))[:]
     deltas = list(map(lambda i : (regionpt[i] - point[i]), r))
     return deltas
   def epsilonModifyMagnitude(self, oldvector, epsilon):
     newvector = list(map(lambda x : x * (epsilon ** self.j), oldvector))
     return newvector
+  def calculateAvoidanceVector(self, vec_a, epsilon):
+    v_delta_b_0 = list(map(lambda x : x * ((epsilon ** self.j) / 2), vec_a))
+    return self.vectorSum(vec_a, v_delta_b_0)
   def vectorSum(self, a, b):
     r = list(range(self.dims))[:]
     return list(map(lambda i : (a[i] + b[i]), r))
